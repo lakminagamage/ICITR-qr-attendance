3172e8a2031dc230c5a5f75a85765f836ed5c8df
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2024-12-05 16:41:53 +0530

    build apk

--- qr_mobile_app/ICITR-QR/index.js
+++ qr_mobile_app/ICITR-QR/index.js
@@ -2,4 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './App'; // Make sure App.js is in the same directory
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('main', () => App);
